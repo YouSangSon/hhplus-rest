1929d390c24f2771006cec7a7876195cc00c169c
yss911001 <yss911001@gmail.com>
2024-10-25 01:17:10 +0900

    * test

--- src/main/kotlin/yousang/rest_server/security/JwtTokenProvider.kt
+++ src/main/kotlin/yousang/rest_server/security/JwtTokenProvider.kt
@@ -16,7 +16,6 @@ class JwtTokenProvider(
     @Value("\${jwt.secret-key}") private val secretKey: String,
     @Value("\${jwt.access-token-validity}") private val accessTokenValidityInSeconds: Long = 3600 * 24 * 3 * 1000, // 3 days
     @Value("\${jwt.refresh-token-validity}") private val refreshTokenValidityInSeconds: Long = 3600 * 24 * 15 * 1000, // 15 days
-    @Value("\${jwt.queue-token-validity}") private val queueTokenValidityInSeconds: Long = 300 * 1000 // 5 minutes
 ) {
     private val key: SecretKey = Keys.hmacShaKeyFor(secretKey.toByteArray())
 
@@ -78,72 +77,3 @@ class JwtTokenProvider(
         }
     }
 }
-
-//@Component
-//class JwtTokenProvider(
-//    @Value("\${jwt.secret}")
-//    private val secretKey: String
-//) {
-//
-//    private val key: Key = Keys.hmacShaKeyFor(secretKey.toByteArray())
-//    val accessTokenValidity: Long = 1000L * 60 * 30 // 30분
-//    val refreshTokenValidity: Long = 1000L * 60 * 60 * 24 * 7 // 7일
-//
-//    fun generateAccessToken(userId: Long, uuid: String): String {
-//        val claims = Jwts.claims().setSubject(userId.toString())
-//        claims["uuid"] = uuid
-//        claims["type"] = "ACCESS"
-//        val now = Date()
-//        val validity = Date(now.time + accessTokenValidity)
-//
-//        return Jwts.builder()
-//            .setClaims(claims)
-//            .setIssuedAt(now)
-//            .setExpiration(validity)
-//            .signWith(key, SignatureAlgorithm.HS256)
-//            .compact()
-//    }
-//
-//    fun generateRefreshToken(userId: Long, uuid: String): String {
-//        val claims = Jwts.claims().setSubject(userId.toString())
-//        claims["uuid"] = uuid
-//        claims["type"] = "REFRESH"
-//        val now = Date()
-//        val validity = Date(now.time + refreshTokenValidity)
-//
-//        return Jwts.builder()
-//            .setClaims(claims)
-//            .setIssuedAt(now)
-//            .setExpiration(validity)
-//            .signWith(key, SignatureAlgorithm.HS256)
-//            .compact()
-//    }
-//
-//    fun validateToken(token: String): Boolean {
-//        try {
-//            val claims = getClaims(token)
-//            return !claims.expiration.before(Date())
-//        } catch (e: JwtException) {
-//            return false
-//        } catch (e: IllegalArgumentException) {
-//            return false
-//        }
-//    }
-//
-//    fun getAuthentication(token: String): Authentication {
-//        val claims = getClaims(token)
-//        val userId = claims.subject.toLong()
-//        val uuid = claims["uuid"] as String
-//
-//        val principal = CustomUserDetails(userId, uuid)
-//        return UsernamePasswordAuthenticationToken(principal, "", principal.authorities)
-//    }
-//
-//    fun getClaims(token: String): Claims {
-//        return Jwts.parserBuilder()
-//            .setSigningKey(key)
-//            .build()
-//            .parseClaimsJws(token)
-//            .body
-//    }
-//}
